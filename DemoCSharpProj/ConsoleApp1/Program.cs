b03d85b99d06832d02412f71834325ae99701b34
Sebastien Siva <ssiva@ggc.edu>
2025-08-12 23:13:27 +0000

    Lecture 8_12_25

--- DemoCSharpProj/ConsoleApp1/Program.cs
+++ DemoCSharpProj/ConsoleApp1/Program.cs
@@ -4,8 +4,35 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello!");
-        Fruit f = new Fruit("Pear", 1.05);
-        Console.WriteLine(f);
+        /*
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Hello!");
+        }
+        */
+        Console.Write("Enter name: ");
+        string n = Console.ReadLine();
+        Console.WriteLine("Hi " + n + " my name is HAL!");
+
+        int[] a = new int[7];
+        a[0] = 9001;
+        a[6] = 345;
+        for (int i = 0; i < a.Length; i++)
+        {
+            Console.WriteLine(a[i]);
+        }
+
+        string[,] g = new string[4, 4];
+        g[0, 2] = "Y";
+        
+
+
+        //Fruit f = new Fruit("Pear", 1.05);
+        //Console.WriteLine(f);
+
+        //int x = 234;
+        //string s = "3kwejroiwejr welrjw erlkjwe rlkwejr ";
+
+
     }
 }
